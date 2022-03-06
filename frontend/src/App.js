a3997f2e9366653097a93b5ef8af9cae01f8bb0e
E N D E R <enderit1102@gmail.com>
2022-03-06 19:43:38 +0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="/">Amazona</a>
+      </header>
+
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
