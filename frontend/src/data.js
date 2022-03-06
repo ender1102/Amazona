ae6d42ef6740b1565d747b3d4955cc173f49f901
E N D E R <enderit1102@gmail.com>
2022-03-06 23:04:02 +0700

    list product

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', //679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Adidas Slim shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Nike Slim pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Adidas Slim pant',
+            slug: 'adidas-slim-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+    ]
+}
+
+export default data;
