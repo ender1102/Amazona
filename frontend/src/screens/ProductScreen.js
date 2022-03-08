2f7e09bcbc2175417b50ce876c1d9e7a43bf96a4
E N D E R <enderit1102@gmail.com>
2022-03-09 02:47:50 +0700

    Create Store context

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import Row from 'react-bootstrap/Row'
@@ -12,6 +12,7 @@ import { Helmet } from 'react-helmet-async'
 import LoadingBox from './../components/LoadingBox'
 import MessageBox from './../components/MessageBox'
 import { getError } from './../utils'
+import { Store } from '../Store'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -43,13 +44,20 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`)
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        console.log('err ', err);
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
       //setProducts(result.data);
     };
     fetchData();
   }, [slug])
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity: 1 }
+    })
+  }
 
   return (
     loading ? <LoadingBox />
@@ -97,7 +105,7 @@ function ProductScreen() {
                     {product.countInStock > 0 &&
                       <ListGroup.Item>
                         <div className='d-grid'>
-                          <Button variant="primary">
+                          <Button variant="primary" onClick={addToCartHandler}>
                             Add to Cart
                           </Button>
                         </div>
