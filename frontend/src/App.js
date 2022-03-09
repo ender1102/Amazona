77bf7cea42044cdfd9619108c2c1f20c130fb9b1
E N D E R <enderit1102@gmail.com>
2022-03-09 18:17:42 +0700

    improve Add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
                 Cart
                 {cart.cartItems.length > 0 && (
                   <Badge pill bg="danger">
-                    {cart.cartItems.length}
+                    {cart.cartItems.reduce((total, current) => total + current.quantity, 0)}
                   </Badge>
                 )}
               </Link>
