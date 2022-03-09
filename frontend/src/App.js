ff74594f654b88b6b9023a844d77b8cb15df6497
E N D E R <enderit1102@gmail.com>
2022-03-09 18:52:20 +0700

    create Cart Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Container from 'react-bootstrap/Container'
 import {LinkContainer} from 'react-router-bootstrap'
 import Badge from 'react-bootstrap/esm/Badge'
 import { Store } from './Store'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<HomeScreen />} />
             <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
           </Routes>
         </Container>
         </main>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer, useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -28,6 +28,7 @@ const reducer = (state, action) => {
 }
 
 function ProductScreen() {
+  const navigate = useNavigate()
   const params = useParams()
   const { slug } = params
 
@@ -61,6 +62,7 @@ function ProductScreen() {
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity }
     })
+    navigate('/cart')
   }
 
   return (
