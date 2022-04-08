d927668b413f758258758798483342154a82c798
E N D E R <enderit1102@gmail.com>
2022-04-09 00:43:46 +0700

    Create Product (API)

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,9 +1,14 @@
 import Axios from 'axios'
 import React, { useContext, useEffect, useReducer } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import { Store } from '../Store'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import { toast } from 'react-toastify'
+import { getError } from './../utils'
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -19,18 +24,27 @@ const reducer = (state, action) => {
             }
         case 'FETCH_FAIL':
             return { ...state, loading: false, error: action.payload }
-
+        case 'CREATE_REQUEST':
+            return { ...state, loadingCreate: true };
+        case 'CREATE_SUCCESS':
+            return {
+                ...state,
+                loadingCreate: false,
+            };
+        case 'CREATE_FAIL':
+            return { ...state, loadingCreate: false };
         default:
             return state
     }
 }
 
 export default function ProductListScreen() {
-    const [{ loading, error, products, pages }, dispatch] = useReducer(reducer, {
+    const [{ loading, error, products, pages, loadingCreate }, dispatch] = useReducer(reducer, {
         loading: true,
         error: ''
     })
 
+    const navigate = useNavigate()
     const { search, pathname } = useLocation()
     const sp = new URLSearchParams(search)
     const page = sp.get('page') || 1
@@ -52,9 +66,36 @@ export default function ProductListScreen() {
         fetchData()
     }, [page, userInfo])
 
+    const createHandler = async () => {
+        if (window.confirm('Are you sure to create?')) {
+            try {
+                dispatch({ type: 'CREATE_REQUEST' })
+                const { data } = await Axios.post('/api/products', {}, {
+                    headers: { Authorization: `Bearer ${userInfo.token}` }
+                })
+                toast.success('product created successfully')
+                dispatch({ type: 'CREATE_SUCCESS' })
+                navigate(`/admin/product/${data.product._id}`)
+            } catch (err) {
+                toast.error(getError(error))
+                dispatch({ type: 'CREATE_FAIL' })
+            }
+        }
+    }
+
     return (
         <div>
-            <h1>Products</h1>
+            <Row>
+                <Col><h1>Products</h1></Col>
+                <Col className="col text-end">
+                    <div>
+                        <Button type="button" onClick={createHandler}>
+                            Create Product
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
+            {loadingCreate && <LoadingBox></LoadingBox>}
             {loading ? (<LoadingBox />)
                 : error ? (<MessageBox variant="danger">{error}</MessageBox>)
                     : <>
@@ -66,7 +107,6 @@ export default function ProductListScreen() {
                                     <th>PRICE</th>
                                     <th>CATEGORY</th>
                                     <th>BRAND</th>
-                                    <th>ACTION</th>
                                 </tr>
                             </thead>
                             <tbody>
