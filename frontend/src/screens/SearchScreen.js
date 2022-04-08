6133df63e56b1e6684b843b665a000f958782793
E N D E R <enderit1102@gmail.com>
2022-04-08 23:59:37 +0700

    Create Dashboard Screen (API)

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -175,60 +175,56 @@ export default function SearchScreen() {
                     </div>
                 </Col>
                 <Col md={9}>
-                    {loading ? (
-                        <LoadingBox></LoadingBox>
-                    ) : error ? (
-                        <MessageBox variant="danger">{error}</MessageBox>
-                    ) : (
-                        <>
-                            <Row className="justify-content-between mb-3">
-                                <Col md={6}>
-                                    <div>
-                                        {countProducts === 0 ? 'No' : countProducts} Results
-                                        {query !== 'all' && ' : ' + query}
-                                        {category !== 'all' && ' : ' + category}
-                                        {price !== 'all' && ' : Price ' + price}
-                                        {rating !== 'all' && ' : Rating ' + rating + ' & up'}
-                                        {query !== 'all' || category !== 'all' || rating !== 'all' || price !== 'all' ? (
-                                            <Button variant="light" onClick={() => navigate('/search')}>
-                                                <i className="fas fa-times-circle"></i>
-                                            </Button>
-                                        ) : null}
-                                    </div>
-                                </Col>
-                                <Col className="text-end">
-                                    Sort by{' '}
-                                    <select value={order} onChange={(e) => {
-                                        navigate(getFilterUrl({ order: e.target.value }))
-                                    }}>
-                                        <option value="newest">Newest Arrivals</option>
-                                        <option value="lowest">Price: Low to High</option>
-                                        <option value="highest">Price: High to Low</option>
-                                        <option value="toprated">Avg. Customer Reviews</option>
-                                    </select>
-                                </Col>
-                            </Row>
-                            {products.length === 0 && (
-                                <MessageBox>No Product Found</MessageBox>
-                            )}
-                            <Row>
-                                {products.map((product) => (
-                                    <Col sm={6} lg={4} className="mb-3" key={product._id}>
-                                        <Product product={product}></Product>
+                    {loading ? <LoadingBox></LoadingBox>
+                        : error ? <MessageBox variant="danger">{error}</MessageBox>
+                            : <>
+                                <Row className="justify-content-between mb-3">
+                                    <Col md={6}>
+                                        <div>
+                                            {countProducts === 0 ? 'No' : countProducts} Results
+                                            {query !== 'all' && ' : ' + query}
+                                            {category !== 'all' && ' : ' + category}
+                                            {price !== 'all' && ' : Price ' + price}
+                                            {rating !== 'all' && ' : Rating ' + rating + ' & up'}
+                                            {query !== 'all' || category !== 'all' || rating !== 'all' || price !== 'all' ? (
+                                                <Button variant="light" onClick={() => navigate('/search')}>
+                                                    <i className="fas fa-times-circle"></i>
+                                                </Button>
+                                            ) : null}
+                                        </div>
                                     </Col>
-                                ))}
-                            </Row>
-                            <div>
-                                {[...Array(pages).keys()].map((x) => (
-                                    <LinkContainer key={x + 1} className="mx-1" to={getFilterUrl({ page: x + 1 })}>
-                                        <Button className={Number(page) === x + 1 ? 'text-bold' : ''} variant="light">
-                                            {x + 1}
-                                        </Button>
-                                    </LinkContainer>
-                                ))}
-                            </div>
-                        </>
-                    )}
+                                    <Col className="text-end">
+                                        Sort by{' '}
+                                        <select value={order} onChange={(e) => {
+                                            navigate(getFilterUrl({ order: e.target.value }))
+                                        }}>
+                                            <option value="newest">Newest Arrivals</option>
+                                            <option value="lowest">Price: Low to High</option>
+                                            <option value="highest">Price: High to Low</option>
+                                            <option value="toprated">Avg. Customer Reviews</option>
+                                        </select>
+                                    </Col>
+                                </Row>
+                                {products.length === 0 && (
+                                    <MessageBox>No Product Found</MessageBox>
+                                )}
+                                <Row>
+                                    {products.map((product) => (
+                                        <Col sm={6} lg={4} className="mb-3" key={product._id}>
+                                            <Product product={product}></Product>
+                                        </Col>
+                                    ))}
+                                </Row>
+                                <div>
+                                    {[...Array(pages).keys()].map((x) => (
+                                        <LinkContainer key={x + 1} className="mx-1" to={getFilterUrl({ page: x + 1 })}>
+                                            <Button className={Number(page) === x + 1 ? 'text-bold' : ''} variant="light">
+                                                {x + 1}
+                                            </Button>
+                                        </LinkContainer>
+                                    ))}
+                                </div>
+                            </>}
                 </Col>
             </Row>
         </div>
