5bc0ccad1e711ab81a4511360c2535a024c5d6c4
E N D E R <enderit1102@gmail.com>
2022-04-09 01:21:53 +0700

    Create Product Edit Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 import DashboardScreen from './screens/DashboardScreen'
 import AdminRoute from './components/AdminRoute'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -178,6 +179,11 @@ function App() {
               <Route path="/admin/products" element={
                 <AdminRoute>
                   <ProductListScreen />
+                </AdminRoute>
+              } />
+              <Route path="/admin/product/:id" element={
+                <AdminRoute>
+                  <ProductEditScreen />
                 </AdminRoute>
               } />
 
