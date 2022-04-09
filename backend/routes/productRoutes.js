a8596047251b393004e2918b64bbf97ddc675886
E N D E R <enderit1102@gmail.com>
2022-04-09 17:41:03 +0700

    delete product (API)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -45,6 +45,17 @@ productRouter.put('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) 
   }
 }))
 
+productRouter.delete('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+  console.log(req)
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    await product.remove()
+    res.send({ message: 'Product Deleted' })
+  } else {
+    res.status(404).send({ message: 'Product Not Found' })
+  }
+}))
+
 const PAGE_SIZE = 3
 
 productRouter.get('/admin', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -10,6 +10,7 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import { toast } from 'react-toastify'
 import { getError } from './../utils'
+import axios from 'axios'
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -26,21 +27,33 @@ const reducer = (state, action) => {
         case 'FETCH_FAIL':
             return { ...state, loading: false, error: action.payload }
         case 'CREATE_REQUEST':
-            return { ...state, loadingCreate: true };
+            return { ...state, loadingCreate: true }
         case 'CREATE_SUCCESS':
             return {
                 ...state,
                 loadingCreate: false,
             };
         case 'CREATE_FAIL':
-            return { ...state, loadingCreate: false };
+            return { ...state, loadingCreate: false }
+        case 'DELETE_REQUEST':
+            return { ...state, loadingDelete: true, successDelete: false }
+        case 'DELETE_SUCCESS':
+            return {
+                ...state,
+                loadingDelete: false,
+                successDelete: true
+            };
+        case 'DELETE_FAIL':
+            return { ...state, loadingDelete: false, successDelete: false }
+        case 'DELETE_RESET':
+            return { ...state, loadingDelete: false, successDelete: false }
         default:
             return state
     }
 }
 
 export default function ProductListScreen() {
-    const [{ loading, error, products, pages, loadingCreate }, dispatch] = useReducer(reducer, {
+    const [{ loading, error, products, pages, loadingCreate, loadingDelete, successDelete }, dispatch] = useReducer(reducer, {
         loading: true,
         error: ''
     })
@@ -64,8 +77,12 @@ export default function ProductListScreen() {
 
             }
         }
-        fetchData()
-    }, [page, userInfo])
+        if (successDelete) {
+            dispatch({ type: 'DELETE_RESET' })
+        } else {
+            fetchData()
+        }
+    }, [page, userInfo, successDelete])
 
     const createHandler = async () => {
         if (window.confirm('Are you sure to create?')) {
@@ -84,6 +101,23 @@ export default function ProductListScreen() {
         }
     }
 
+    const deleteHandler = async (product) => {
+        if (window.confirm('Are you sure to create?')) {
+            try {
+                dispatch({ type: 'DELETE_REQUEST' })
+                await Axios.delete(`/api/products/${product._id}`, {
+                    headers: { Authorization: `Bearer ${userInfo.token}` }
+                })
+                toast.success('product created successfully')
+                dispatch({ type: 'DELETE_SUCCESS' })
+
+            } catch (err) {
+                toast.error(getError(error))
+                dispatch({ type: 'DELETE_FAIL' })
+            }
+        }
+    }
+
     return (
         <div>
             <Row>
@@ -97,6 +131,7 @@ export default function ProductListScreen() {
                 </Col>
             </Row>
             {loadingCreate && <LoadingBox></LoadingBox>}
+            {loadingDelete && <LoadingBox></LoadingBox>}
             {loading ? (<LoadingBox />)
                 : error ? (<MessageBox variant="danger">{error}</MessageBox>)
                     : <>
@@ -121,6 +156,8 @@ export default function ProductListScreen() {
                                         <td>{product.brand}</td>
                                         <td>
                                             <Button type="button" variant="light" onClick={() => navigate(`/admin/product/${product._id}`)}>Edit</Button>
+                                            &nbsp;
+                                            <Button type="button" variant="light" onClick={() => deleteHandler(product)}>Delete</Button>
                                         </td>
                                     </tr>
                                 ))}
