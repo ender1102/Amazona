f82010b9c72f59492ce36a35dfbaeb6ec9fb19c8
E N D E R <enderit1102@gmail.com>
2022-04-14 01:05:26 +0700

    User List, User Edit Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,8 @@ import AdminRoute from './components/AdminRoute'
 import ProductListScreen from './screens/ProductListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
 import OrderListScreen from './screens/OrderListScreen'
+import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -190,6 +192,16 @@ function App() {
               <Route path="/admin/orders" element={
                 <AdminRoute>
                   <OrderListScreen />
+                </AdminRoute>
+              } />
+              <Route path="/admin/users" element={
+                <AdminRoute>
+                  <UserListScreen />
+                </AdminRoute>
+              } />
+              <Route path="/admin/user/:id" element={
+                <AdminRoute>
+                  <UserEditScreen />
                 </AdminRoute>
               } />
 
