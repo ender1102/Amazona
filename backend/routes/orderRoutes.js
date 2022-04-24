f811ee5601337ebdff52761a6cbdd51df3ddb5ca
E N D E R <enderit1102@gmail.com>
2022-04-24 13:22:25 +0700

    send mail with mailgun (BE)

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js';
 import Product from '../models/productModel.js';
 import User from '../models/userModel.js';
-import { isAuth, isAdmin } from '../utils.js';
+import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from '../utils.js';
 
 const orderRouter = express.Router();
 
@@ -96,7 +96,10 @@ orderRouter.put('/:id/deliver', isAuth, expressAsyncHandler(async (req, res) => 
 }))
 
 orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id)
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'email name'
+    )
     if (order) {
         order.isPaid = true
         order.paidAt = Date.now()
@@ -108,6 +111,19 @@ orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) => {
         }
 
         const updateOrder = await order.save()
+        mailgun().messages().send({
+            from:'Amazona <amazona@mg.yourdomain.com>',
+            to: `${order.user.name} <${order.user.email}>`,
+            subject: `New order ${order._id}`,
+            html:payOrderEmailTemplate(order)
+        },
+        (error, body) => {
+            if(error){
+                console.log(error)
+            }else{
+                console.log(body)
+            }
+        })
         res.send({ message: 'Order Paid', order: updateOrder })
     } else {
         res.status(404).send({ message: 'Order Not Found' })
